c21fc63ceac91cddfcb57348c8ef2e861907e0bb
Melovii <145908254+Melovii@users.noreply.github.com>
2024-02-26 11:44:33 +0300

    Finally finished everything with the code, changed the winning conditions implementation.

--- src/BroCode/myWindow.java
+++ src/BroCode/myWindow.java
@@ -1,50 +1,31 @@
 package BroCode;
 
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class myWindow extends JFrame {
+public class myWindow extends JFrame implements ActionListener {
 
-    JButton[] boxes = new JButton[9];
+    JButton[] box = new JButton[9];
     JPanel TitlePanel = new JPanel();
-    JPanel GridPanel = new JPanel();
     JLabel BottomTitle = new JLabel();
+    JPanel GridPanel = new JPanel();
 
+    public int[] slots = new int[9];
 
-    public int[] slots = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    boolean player1_turn;
+    int counter = 0;
 
+    // Winning patterns
+    int[][] winningPatterns = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // horizontal
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // vertical
+            {0, 4, 8}, {2, 4, 6}              // diagonal
+    };
+
+    // Window settings
     myWindow() {
-        for (int i = 0; i < boxes.length; i++) {
-            final int INDEX = i;
-            boxes[i] = new JButton();
-            boxes[i].setFont(new Font("Calibri", Font.PLAIN, 69));
-            boxes[i].setFocusable(false);
-            GridPanel.add(boxes[i]);
-            boxes[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (boxes[INDEX].getText().isEmpty())
-                    {
-                        if ((counter % 2) == 1) // if counter is odd, it's O's turn
-                        {
-                            boxes[INDEX].setText(user1);
-                            slots[INDEX] = 1;
-                        } else // if counter is even, it's X's turn
-                        {
-                            boxes[INDEX].setText(user2);
-                            slots[INDEX] = 0;
-                        }
-                    }
-                    counter++;
-                    checkDraw();
-                    checkWinner();
-                }
-            });
-        }
-
-        // window settings
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(420, 520);
         this.getContentPane().setBackground(Color.darkGray);
@@ -65,105 +46,73 @@ public class myWindow extends JFrame {
 
         GridPanel.setLayout(new GridLayout(3, 3));
 
+        for (int i = 0; i < 9; i++) {
+            box[i] = new JButton();
+            box[i].setFont(new Font("Calibri", Font.PLAIN, 69));
+            box[i].setFocusable(false);
+            box[i].addActionListener(this);
+            GridPanel.add(box[i]);
+        }
+
         this.add(GridPanel);
         this.add(BottomTitle, BorderLayout.NORTH);
         this.setVisible(true);
-
     }
 
-    String user1 = "O";
-    String user2 = "X";
-    int counter = 0;
-
-    public void whoWins() {
-        boolean flag = true;
-        if ((counter % 2) == 1) // if counter is odd, it's X's turn
-        {
-            BottomTitle.setText("X wins!");
-            flag = false;
-        }
-        else if ((counter % 2 == 0))// if counter is even, it's O's turn
-        {
-            BottomTitle.setText("O wins!");
-            flag = false;
-        }
-        if (flag == false)
-        {
-            for (int j=0; j<9; j++)
-            {
-                boxes[j].setEnabled(false);
-            }
-        }
+    public void checkTurn() {
+        player1_turn = counter % 2 == 0;
+        counter++;
     }
 
-    public void checkDraw()
-    {
-        boolean empty_flag = false;
-        for (int i=0; i<9; i++)
-        {
-            if (boxes[i].getText().isEmpty())
-            {
-                empty_flag = true;
+    public boolean checkWin() {
+        for (int[] pattern : winningPatterns) {
+            if (box[pattern[0]].getText().equals(box[pattern[1]].getText()) &&
+                    box[pattern[1]].getText().equals(box[pattern[2]].getText()) &&
+                    !box[pattern[0]].getText().isEmpty()) {
+                // Highlight winning pattern
+                for (int i : pattern) {
+                    box[i].setBackground(Color.GREEN);
+                }
+                return true;
             }
         }
-        if (empty_flag == false)
-        {
-            for (int j=0; j<9; j++)
-            {
-                boxes[j].setEnabled(false);
-            }
-            BottomTitle.setText("It's a draw!");
-        }
+        return false;
     }
 
+    public boolean checkDraw() {
+        for (int i = 0; i < 9; i++) {
+            if (box[i].getText().isEmpty()) {
+                return false; // If any button is empty, game is not draw
+            }
+        }
+        return true; // All buttons filled, game is draw
+    }
 
-    public void checkWinner()
-    {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 9; i++) {
+            if (e.getSource() == box[i]) {
+                if (box[i].getText().isEmpty()) {
+                    checkTurn();
+                    if (player1_turn) {
+                        box[i].setText("X");
+                    } else {
+                        box[i].setText("O");
+                    }
 
-        // horizontal line conditions
-        for (int i = 0; i<9; i += 3) {
-            if (slots[i] == slots[i + 1] && slots[i + 1] == slots[i + 2]) {
-                whoWins();
-                for (int j = i; j<3; j++)
-                {
-                    boxes[j].setFont(new Font("Calibri", Font.BOLD, 69));
-                    boxes[j].setBackground(Color.GREEN);
+                    if (checkWin()) {
+                        BottomTitle.setText(player1_turn ? "Player X wins!" : "Player O wins!");
+                        for (JButton button : box) {
+                            button.setEnabled(false);
+                        }
+                    } else if (checkDraw()) {
+                        BottomTitle.setText("It's a draw!");
+                        for (JButton button : box) {
+                            button.setEnabled(false);
+                        }
+                    }
                 }
             }
         }
-
-        // vertical line conditions
-        for (int i=0; i<3; i++)
-        {
-            if (slots[i] == slots[i + 3] && slots[i + 3] == slots[i + 6]) {
-                whoWins();
-                for (int j = i; j<9; j += 3)
-                {
-                    boxes[j].setFont(new Font("Calibri", Font.BOLD, 69));
-                    boxes[j].setBackground(Color.GREEN);
-                }
-            }
-        }
-
-        // diagonal line conditions
-        if (slots[0] == slots[4] && slots[4] == slots[8])
-        {
-            whoWins();
-            for (int i=0; i<=8; i += 4)
-            {
-                boxes[i].setFont(new Font("Calibri", Font.BOLD, 69));
-                boxes[i].setBackground(Color.GREEN);
-            }
-        }
-        if (slots[2] == slots[4] && slots[4] == slots[6])
-        {
-            whoWins();
-            for (int i=2; i<=6; i += 2)
-            {
-                boxes[i].setFont(new Font("Calibri", Font.BOLD, 69));
-                boxes[i].setBackground(Color.GREEN);
-            }
-        }
-        // end of method
     }
 }
